3b6a5732500f42f94d32c8e4b82fed4dabf20a2a
sumopon <>
2026-03-07 01:47:56 +0900

    データの送信、保存機能の実装とリファクタリング

--- step10-daily-report/src/main.js
+++ step10-daily-report/src/main.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
+import { fetchHistoryData } from "../my-modules/fetch-history-data";
+import { submitData } from "../my-modules/submit-data";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA4L8Pn0VE_EF5Gff2XlieoX4ZDJh9AnCY",
-  authDomain: "daily-report-37acf.firebaseapp.com",
-  projectId: "daily-report-37acf",
-  storageBucket: "daily-report-37acf.firebasestorage.app",
-  messagingSenderId: "143148449655",
-  appId: "1:143148449655:web:5665304bff8d292f7deb72"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID
 };
 
 // Initialize Firebase
@@ -19,19 +18,10 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-const fetchHistoryData = async () => {
-      let tags = "";
-
-      const querySnapshot = await getDocs(collection(db, "reports"));
-
-      querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data()}`);
-            tags += `<tr><td>${doc.data().data}</td><td>${doc.data().name}</td>
-            <td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>`
-      });
-      document.getElementById("js-history").innerHTML = tags;
-};
-
 if (document.getElementById("js-history")) {
       fetchHistoryData();
 }
+
+if (document.getElementById("js-form")) {
+      document.getElementById("js-form").addEventListener("submit", (e) => submitData(e));
+};
